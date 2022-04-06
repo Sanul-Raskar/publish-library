16730096683a17e213c511624dae7c1e2a110d7c
Sanul Raskar <sanulraskar@gmail.com>
2022-04-07 00:34:27 +0530

    updated

--- publish.js
+++ publish.js
@@ -3,7 +3,6 @@ const inquirer = require("inquirer");
 
 inquirer
   .prompt([
-    /* Pass your questions in here */
     {
       type: "list",
       name: "changeType",
@@ -20,26 +19,22 @@ inquirer
     },
   ])
   .then(({ changeType }) => {
-    // Use user feedback for... whatever!!
-
     exec(
-      `npm version --no-git-tag-version ${changeType}`,
+      `npm version --no-git-tag-version ${changeType} && npm pkg get version`,
       (error, stdout, stderr) => {
-        if (error !== null) {
-          //run npm run build command here
-
-          exec("npm pkg get version", (error, stdout, stderr) => {
-            if (error !== null) {
+        if (error === null) {
+          exec("npm run build", (error, stdout, stderr) => {
+            if (error === null) {
+              const tagName = `designsystem@v${stdout.replaceAll('"', "")}`;
               exec(
-                `git commit -m 'chore: Build for v${stdout}'`,
+                `git add lib/* && git commit -m 'chore: Build for v${stdout}'`,
                 (error, stdout, stderr) => {
-                  if (error !== null) {
-                    const tagName = `designsystem@v${stdout}`;
-
+                  if (error === null) {
                     exec(
-                      `git tag ${tagName} && git push origin ${tagName}`,
+                      `git tag ${tagName} && git push origin ${tagName} && git checkout ${tagName}`,
                       (error, stdout, stderr) => {
-                        if (error !== null) {
+                        if (error === null) {
+                          //publish the tag to npm registry
                         }
                       }
                     );
